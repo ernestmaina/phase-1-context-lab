7defe7aab5ec9df2bcf1bfdc023ff37610906480
ErnestMaina <you@example.com>
2023-10-05 14:51:55 +0300

    updated

--- index.js
+++ index.js
@@ -21,3 +21,82 @@ const allWagesFor = function () {
     return payable
 }
 
+
+// Function to create an employee record
+function createEmployeeRecord(array) {
+    return {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  // Function to create employee records from an array of arrays
+  function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+  }
+  
+  // Function to create a timeIn event
+  function createTimeInEvent(employeeRecord, dateTimeString) {
+    const [date, time] = dateTimeString.split(' ');
+    const [hour, minute] = time.split(':');
+  
+    const timeInEvent = {
+      type: 'TimeIn',
+      hour: parseInt(hour, 10),
+      date: date
+    };
+  
+    employeeRecord.timeInEvents.push(timeInEvent);
+  
+    return employeeRecord;
+  }
+  
+  // Function to create a timeOut event
+  function createTimeOutEvent(employeeRecord, dateTimeString) {
+    const [date, time] = dateTimeString.split(' ');
+    const [hour, minute] = time.split(':');
+  
+    const timeOutEvent = {
+      type: 'TimeOut',
+      hour: parseInt(hour, 10),
+      date: date
+    };
+  
+    employeeRecord.timeOutEvents.push(timeOutEvent);
+  
+    return employeeRecord;
+  }
+  
+  // Function to calculate hours worked on a specific date
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeIn = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOut = employeeRecord.timeOutEvents.find(event => event.date === date);
+  
+    if (!timeIn || !timeOut) {
+      return undefined; // If timeIn or timeOut event is missing, return undefined
+    }
+  
+    const hoursWorked = (timeOut.hour - timeIn.hour) / 100; // Calculate hours worked
+  
+    return hoursWorked;
+  }
+  
+  // Function to calculate wages earned on a specific date
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+  
+    if (hoursWorked === undefined) {
+      return 0; // No matching timeIn or timeOut event found for the date
+    }
+  
+    const ratePerHour = employeeRecord.payPerHour;
+    const wagesEarned = hoursWorked * ratePerHour;
+  
+    return wagesEarned;
+  }
+
+ 
